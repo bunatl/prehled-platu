3df1ee135de1e76a8d7cb80437d3e90af5e1a52e
bunatl <lukas.bunat@gmail.com>
2020-09-03 23:38:56 +0200

    API DB insert working

--- server/src/api/Salary.js
+++ server/src/api/Salary.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const SalaryEntrySchema = require('../model/SalarySchema');
 
-
 // == DB connection == 
 const db = require('monk')('localhost/salaries-cz');
 // get or create a collection
@@ -11,12 +10,22 @@ const salaries = db.get('salaries');
 
 router.post('/', async (req, res, next) => {
     try {
+        // validate input
         const result = await SalaryEntrySchema.validateAsync(req.body);
-
         // search all
-        const items = await salaries.find({});
-        // insert int DB - watch out for duplicates
-        salaries.insert(result);
+        // const items = await salaries.find({});
+        // search if entry already exists in DB
+        await salaries.findOne(result)
+            .then((doc) => {
+                // document with matching criteria or null
+                if (doc !== null) {
+                    console.error('Already added');
+                    res.status(400);
+                    return;
+                }
+            });
+        // if entry is unique, insert into DB
+        await salaries.insert(result);
 
         res.json({
             msg: "validation sucessfull",
@@ -32,5 +41,6 @@ router.post('/', async (req, res, next) => {
     }
     db.close();
 });
+
 
 module.exports = router;
